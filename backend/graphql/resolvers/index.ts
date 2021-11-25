5b6deabde978501fcbba455ed8cec15598bc0ddd
zaini <alimzaini@outlook.com>
2021-11-25 20:06:26 +0000

    Add login and hashing

--- backend/graphql/resolvers/index.ts
+++ backend/graphql/resolvers/index.ts
@@ -1,5 +1,6 @@
 import { prisma } from "../../index";
 import { UserInputError } from "apollo-server-errors";
+const argon2 = require("argon2");
 
 module.exports = {
   Mutation: {
@@ -8,7 +9,10 @@ module.exports = {
         throw new UserInputError("Passwords must match");
       }
       try {
-        const user = prisma.user.create({ data: { email, password } });
+        const hashed_password = await argon2.hash(password);
+        const user = await prisma.user.create({
+          data: { email, password: hashed_password },
+        });
         return user;
       } catch (error) {
         throw new UserInputError(
@@ -16,8 +20,19 @@ module.exports = {
         );
       }
     },
-    login: (data: any) => {
-      console.log(data);
+    login: async (_: any, { email, password }: any) => {
+      const user = await prisma.user.findUnique({ where: { email } });
+      if (!user) {
+        throw new UserInputError("User with this email could not be found");
+      }
+      const is_password_valid: boolean = await argon2.verify(
+        user.password,
+        password
+      );
+      if (!is_password_valid) {
+        throw new UserInputError("Invalid password");
+      }
+      return { accessToken: "VALID" };
     },
   },
   Query: {

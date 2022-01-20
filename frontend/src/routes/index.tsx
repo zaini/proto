e1ec24b0855733c819cd08a168eab9eafc7bcd09
zaini <alimzaini@outlook.com>
2022-01-20 21:49:41 +0000

    Disable ProtectedRoutes due to bugs

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,45 +5,17 @@ import DashboardHome from "../views/Dashboard/Home/Home";
 import Home from "../views/Home/Home";
 import NotFound from "../views/NotFound/NotFound";
 import Problem from "../views/Problem/Problem";
-import ProtectedRoute from "./utils/ProtectedRoute";
+// import ProtectedRoute from "./utils/ProtectedRoute";
 
 const IndexRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path={"/"}
-          element={
-            <ProtectedRoute mustBeLoggedIn={false}>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path={"/accounts/login"}
-          element={
-            <ProtectedRoute mustBeLoggedIn={false}>
-              <Login />
-            </ProtectedRoute>
-          }
-        />
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/accounts/login"} element={<Login />} />
         <Route path={"/accounts/log-out"} element={<Logout />} />
-        <Route
-          path={"/dashboard/"}
-          element={
-            <ProtectedRoute mustBeLoggedIn={true}>
-              <DashboardHome />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path={"/problems/:problemId"}
-          element={
-            <ProtectedRoute mustBeLoggedIn={true}>
-              <Problem />
-            </ProtectedRoute>
-          }
-        />
+        <Route path={"/dashboard/"} element={<DashboardHome />} />
+        <Route path={"/problems/:problemId"} element={<Problem />} />
         <Route path={"*"} element={<NotFound />} />
       </Routes>
     </BrowserRouter>

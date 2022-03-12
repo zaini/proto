c23e300ab5d08a0d0ca7697aff797cc5dba463fe
zaini <alimzaini@outlook.com>
2022-03-12 03:15:50 +0000

    Add queries for dashboard

--- frontend/src/views/Dashboard/Home/Home.tsx
+++ frontend/src/views/Dashboard/Home/Home.tsx
@@ -1,24 +1,18 @@
 import React from "react";
 import { Box, VStack } from "@chakra-ui/react";
-import ProblemCatalogues from "../../../components/Dashboard/ProblemCatalogues/ProblemCatalogues";
 import ProblemTable from "../../../components/ProblemTable/ProblemTable";
 import AssignmentsPreview from "../../../components/Dashboard/AssignmentsPreview/AssignmentsPreview";
 
 const DashboardHome = () => {
   return (
-    <Box>
-      <VStack align="stretch">
-        <Box bgColor="#C4C4C4" px={"12.5%"} py={12}>
-          <AssignmentsPreview />
-        </Box>
-        <Box px={"12.5%"} py={12}>
-          <ProblemCatalogues />
-        </Box>
-        <Box px={"12.5%"} pt={12}>
-          <ProblemTable />
-        </Box>
-      </VStack>
-    </Box>
+    <VStack align="stretch">
+      <Box bgColor="#C4C4C4" px={"12.5%"} py={12}>
+        <AssignmentsPreview />
+      </Box>
+      <Box px={"12.5%"} pt={12}>
+        <ProblemTable />
+      </Box>
+    </VStack>
   );
 };
 

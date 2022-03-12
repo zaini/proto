96e22d9eb79efc990248bbd0bb89f55a1152e4c3
zaini <alimzaini@outlook.com>
2022-03-12 03:45:48 +0000

    Show assignments on dashboard

---
+++ frontend/src/components/AssignmentBox/AssignmentBox.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Box, Text, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { Assignment } from "../../gql-types";
+
+const AssignmentBox = ({ assignment }: { assignment: Assignment }) => {
+  return (
+    <Box
+      p={2}
+      minw={"100%"}
+      backgroundColor={"#EEEEEE"}
+      fontWeight={700}
+      borderRadius={5}
+      boxShadow={"xl"}
+      boxSizing={"border-box"}
+    >
+      <Text fontSize="xl">{assignment.name}</Text>
+      <Text fontSize="md" opacity={"50%"}>
+        {assignment.classroom.name}
+      </Text>
+      <Text fontSize="md">{`Due on ${new Date(
+        parseInt(assignment.dueDate)
+      ).toLocaleString()}`}</Text>
+      <Box textAlign="right" mt={4}>
+        <Button
+          as={Link}
+          to={`/dashboard/classrooms/${assignment.classroom.id}/assignments/${assignment.id}`}
+          fontSize="lg"
+          backgroundColor={"#00ADB5"}
+        >
+          GO
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default AssignmentBox;
--- frontend/src/components/Dashboard/AssignmentsPreview/AssignmentsPreview.tsx
+++ frontend/src/components/Dashboard/AssignmentsPreview/AssignmentsPreview.tsx
@@ -1,24 +1,54 @@
-import React from "react";
-import { Box, Heading, HStack, Center, Spinner } from "@chakra-ui/react";
-import ContentBox from "../../ContentBox/ContentBox";
+import React, { useRef } from "react";
+import {
+  Box,
+  Heading,
+  HStack,
+  Center,
+  Spinner,
+  IconButton,
+} from "@chakra-ui/react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/client";
+import { Assignment } from "../../../gql-types";
+import AssignmentBox from "../../AssignmentBox/AssignmentBox";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 
 const GET_ASSIGNMENTS = gql`
   query getAssignments {
     getAssignments {
+      id
       dueDate
       createdAt
       name
       classroom {
+        id
         name
       }
     }
   }
 `;
 
+// From: https://codesandbox.io/s/mxe72?file=/src/styles.ts:657-665
+const sideScroll = (
+  element: HTMLDivElement,
+  speed: number,
+  distance: number,
+  step: number
+) => {
+  let scrollAmount = 0;
+  const slideTimer = setInterval(() => {
+    element.scrollLeft += step;
+    scrollAmount += Math.abs(step);
+    if (scrollAmount >= distance) {
+      clearInterval(slideTimer);
+    }
+  }, speed);
+};
+
 const AssignmentsPreview = () => {
   const { loading, error, data } = useQuery(GET_ASSIGNMENTS);
+
+  const contentWrapper = useRef(null);
 
   if (loading)
     return (
@@ -35,22 +65,52 @@ const AssignmentsPreview = () => {
   // TODO have an actual error page and log this
   if (error) return <>Error! {error.message}</>;
 
-  const assignments = data.getAssignments;
+  const assignments: Assignment[] = data.getAssignments;
 
   console.log(assignments);
+
+  if (assignments.length === 0) {
+    return (
+      <Box>
+        <Heading size="lg">You have 0 assignments! 🥳</Heading>
+      </Box>
+    );
+  }
 
   return (
     <Box>
       <Heading size="lg" mb={12}>
-        You have 2 assignments!
+        You have {assignments.length} assignments!
       </Heading>
 
-      <HStack spacing="auto">
-        <ContentBox header="Two Sum" subheader="Easy" text="Due in 3 hours" />
-        <ContentBox header="Two Sum" subheader="Easy" text="Due in 3 hours" />
-        <ContentBox header="Two Sum" subheader="Easy" text="Due in 3 hours" />
-        <ContentBox header="Two Sum" subheader="Easy" text="Due in 3 hours" />
-        <ContentBox header="Two Sum" subheader="Easy" text="Due in 3 hours" />
+      <HStack>
+        <IconButton
+          aria-label="left"
+          icon={<ChevronLeftIcon />}
+          onClick={() => {
+            sideScroll(contentWrapper.current as any, 25, 100, -10);
+          }}
+        />
+
+        <HStack
+          spacing={8}
+          overflow={"hidden"}
+          whiteSpace={"nowrap"}
+          width="100%"
+          ref={contentWrapper}
+        >
+          {assignments.map((assignment) => {
+            return <AssignmentBox assignment={assignment} />;
+          })}
+        </HStack>
+
+        <IconButton
+          aria-label="right"
+          icon={<ChevronRightIcon />}
+          onClick={() => {
+            sideScroll(contentWrapper.current as any, 25, 100, 10);
+          }}
+        />
       </HStack>
     </Box>
   );

3f0b684fd69ed1d0247d3a1ead611ceef5447c41
zaini <alimzaini@outlook.com>
2022-03-14 10:52:21 +0000

    Basic login page redesign

--- frontend/src/views/Accounts/Login/Login.tsx
+++ frontend/src/views/Accounts/Login/Login.tsx
@@ -1,23 +1,38 @@
 import React from "react";
-import { Box, Heading } from "@chakra-ui/layout";
+import { Text, Heading, Stack, Box } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/react";
+import { BsGithub } from "react-icons/bs";
 
 const REACT_APP_GITHUB_AUTH_URL = process.env
   .REACT_APP_GITHUB_AUTH_URL as string;
 
 const Login = () => {
   return (
-    <Box>
-      <Heading>Login</Heading>
-
-      <Button
-        onClick={() => {
-          window.open(REACT_APP_GITHUB_AUTH_URL, "_self");
-        }}
+    <Stack direction={"row"} justify={"space-evenly"}>
+      <Box textAlign={"center"} py={320} w={"100%"}>
+        <Heading>Login to Proto</Heading>
+        <br />
+        <Button
+          aria-label={"Login with GitHub"}
+          leftIcon={<BsGithub />}
+          onClick={() => {
+            window.open(REACT_APP_GITHUB_AUTH_URL, "_self");
+          }}
+        >
+          Login with GitHub
+        </Button>
+      </Box>
+      <Box
+        w={"100%"}
+        py={320}
+        backgroundColor={"#393e46"}
+        textAlign={"center"}
+        color={"#ffffff"}
       >
-        Login with GitHub
-      </Button>
-    </Box>
+        <Heading size={"2xl"}>Proto</Heading>
+        <Text fontSize={"lg"}>Solve and create programming challenges</Text>
+      </Box>
+    </Stack>
   );
 };
 

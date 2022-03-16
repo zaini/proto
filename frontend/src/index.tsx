5d8552f01771f9390172355b5ea84be5e3f0d77f
zaini <alimzaini@outlook.com>
2022-03-16 12:09:52 +0000

    Redesign home page

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./context/ApolloProvider";
 import "./styles/global.css";
+import "./styles/Typist.css";
 
 ReactDOM.render(
   <React.StrictMode>

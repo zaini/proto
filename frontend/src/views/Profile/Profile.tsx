046695feaef9054b783262e84467e83f6b5c2e96
zaini <alimzaini@outlook.com>
2022-03-18 11:24:09 +0000

    Add view submission on profiles

--- frontend/src/views/Profile/Profile.tsx
+++ frontend/src/views/Profile/Profile.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/client";
 import { Problem, Submission, User } from "../../gql-types";
-import { Avatar, Box, Center, Heading, Spinner, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  ButtonGroup,
+  Center,
+  Heading,
+  Spinner,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import HeatMap from "@uiw/react-heat-map";
 import Tooltip from "@uiw/react-tooltip";
 import CustomTable from "../../components/CustomTable/CustomTable";
 import { LanguageCodeToName } from "../../utils";
+import SubmissionModal from "../../components/SubmissionModal/SubmissionModal";
 
 const GET_USER = gql`
   query getUser($userId: ID!) {
@@ -64,6 +75,10 @@ const getSubmissionCounts = (submissions: Submission[]) => {
 const Profile = () => {
   const { userId } = useParams();
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [modalSubmissionId, setModalSubmissionId] = useState<number>(-1);
+
   const { loading, error, data } = useQuery(GET_USER, {
     variables: {
       userId: userId,
@@ -95,149 +110,173 @@ const Profile = () => {
   const problems = profile.problems || [];
 
   return (
-    <Box px={"12.5%"} pt={8}>
-      <Heading size={"2xl"}>
-        <Avatar
-          size={"lg"}
-          src={`https://avatars.githubusercontent.com/u/${profile.githubId}`}
-        />{" "}
-        {profile.username}
-      </Heading>
-      <Heading size={"md"}>
-        Joined on {new Date(parseInt(profile.createdAt)).toLocaleDateString()}
-      </Heading>
-      <br />
-      <Heading size={"lg"}>Recent Submissions</Heading>
-      <br />
-      <Center>
-        <HeatMap
-          rectSize={14}
-          width={600}
-          value={submissionCounts.map(([date, count]: any) => {
-            return { date, count, content: "" };
-          })}
-          panelColors={{
-            0: "#EBEDF0",
-            4: "#8bd8e4",
-            8: "#70b5ba",
-            12: "#5c979c",
-            32: "#467478",
-          }}
-          startDate={dateSixMonthsAgo}
-          rectRender={(props, data) => {
-            if (!data.count || data.count === 0) {
-              return;
-            }
-            return (
-              <Tooltip
-                key={props.key}
-                placement="top"
-                content={`Submissions: ${data.count || 0}`}
-              >
-                <rect {...props} />
-              </Tooltip>
-            );
-          }}
-        />
-      </Center>
-      <br />
-      {submissions.length > 0 ? (
-        <CustomTable
-          data={submissions.map((submission: Submission) => {
-            return {
-              problem: (
-                <a href={`/problems/${submission.problem.id}`}>
-                  #{submission.problem.id}{" "}
-                  {submission.problem.specification.title}
-                </a>
-              ),
-              time: new Date(parseInt(submission.createdAt)).toLocaleString(),
-              passed: `${submission.passed}`,
-              language: LanguageCodeToName[submission.language],
-              avgTime: submission.avgTime.toFixed(2) + " ms",
-              avgMemory: submission.avgMemory.toFixed(2) + " MB",
-            };
-          })}
-          columns={[
-            {
-              Header: "Problem",
-              accessor: "problem",
-            },
-            {
-              Header: "Time",
-              accessor: "time",
-            },
-            {
-              Header: "Passed",
-              accessor: "passed",
-            },
-            {
-              Header: "Average Time",
-              accessor: "avgTime",
-            },
-            {
-              Header: "Average Memory",
-              accessor: "avgMemory",
-            },
-            {
-              Header: "Language",
-              accessor: "language",
-            },
-          ]}
-        />
-      ) : (
-        <Text fontWeight={"bold"}>No Recent Submissions</Text>
-      )}
-      <br />
-      <br />
-      <Heading size={"lg"}>Created Problems</Heading>
-      <br />
-      {problems.length > 0 ? (
-        <CustomTable
-          data={problems.map((problem: Problem) => {
-            return {
-              problem: (
-                <a href={`/problems/${problem.id}`}>
-                  #{problem.id} {problem.specification.title}
-                </a>
-              ),
-              difficulty: problem.specification.difficulty.toLowerCase(),
-              totalRatings: problem.rating.numberOfRatings,
-              avgRating: problem.rating.numberOfRatings
-                ? Math.round(
-                    (problem.rating.totalRating /
-                      problem.rating.numberOfRatings) *
-                      10
-                  ) / 10
-                : "N/A",
-            };
-          })}
-          columns={[
-            {
-              Header: "Problem",
-              accessor: "problem",
-            },
-            {
-              Header: "Difficulty",
-              accessor: "difficulty",
-            },
-            {
-              Header: "Total Ratings",
-              accessor: "totalRatings",
-            },
-            {
-              Header: "Average Rating",
-              accessor: "avgRating",
-            },
-          ]}
-        />
-      ) : (
-        <>
-          <Text fontWeight={"bold"}>No Created Problems</Text>
-          <br />
-        </>
-      )}
-    </Box>
+    <>
+      <SubmissionModal
+        {...{ isOpen, onClose, submissionId: modalSubmissionId }}
+      />
+      <Box px={"12.5%"} pt={8}>
+        <Heading size={"2xl"}>
+          <Avatar
+            size={"lg"}
+            src={`https://avatars.githubusercontent.com/u/${profile.githubId}`}
+          />{" "}
+          {profile.username}
+        </Heading>
+        <Heading size={"md"}>
+          Joined on {new Date(parseInt(profile.createdAt)).toLocaleDateString()}
+        </Heading>
+        <br />
+        <Heading size={"lg"}>Recent Submissions</Heading>
+        <br />
+        <Center>
+          <HeatMap
+            rectSize={14}
+            width={600}
+            value={submissionCounts.map(([date, count]: any) => {
+              return { date, count, content: "" };
+            })}
+            panelColors={{
+              0: "#EBEDF0",
+              4: "#8bd8e4",
+              8: "#70b5ba",
+              12: "#5c979c",
+              32: "#467478",
+            }}
+            startDate={dateSixMonthsAgo}
+            rectRender={(props, data) => {
+              if (!data.count || data.count === 0) {
+                return;
+              }
+              return (
+                <Tooltip
+                  key={props.key}
+                  placement="top"
+                  content={`Submissions: ${data.count || 0}`}
+                >
+                  <rect {...props} />
+                </Tooltip>
+              );
+            }}
+          />
+        </Center>
+        <br />
+        {submissions.length > 0 ? (
+          <CustomTable
+            data={submissions.map((submission: Submission) => {
+              return {
+                problem: (
+                  <a href={`/problems/${submission.problem.id}`}>
+                    #{submission.problem.id}{" "}
+                    {submission.problem.specification.title}
+                  </a>
+                ),
+                time: new Date(parseInt(submission.createdAt)).toLocaleString(),
+                passed: `${submission.passed}`,
+                language: LanguageCodeToName[submission.language],
+                avgTime: submission.avgTime.toFixed(2) + " ms",
+                avgMemory: submission.avgMemory.toFixed(2) + " MB",
+                options: (
+                  <>
+                    <ButtonGroup>
+                      <Button
+                        colorScheme={"blue"}
+                        onClick={() => {
+                          setModalSubmissionId(parseInt(submission.id));
+                          onOpen();
+                        }}
+                      >
+                        View
+                      </Button>
+                    </ButtonGroup>
+                  </>
+                ),
+              };
+            })}
+            columns={[
+              {
+                Header: "Problem",
+                accessor: "problem",
+              },
+              {
+                Header: "Time",
+                accessor: "time",
+              },
+              {
+                Header: "Passed",
+                accessor: "passed",
+              },
+              {
+                Header: "Average Time",
+                accessor: "avgTime",
+              },
+              {
+                Header: "Average Memory",
+                accessor: "avgMemory",
+              },
+              {
+                Header: "Language",
+                accessor: "language",
+              },
+              {
+                Header: "Options",
+                accessor: "options",
+              },
+            ]}
+          />
+        ) : (
+          <Text fontWeight={"bold"}>No Recent Submissions</Text>
+        )}
+        <br />
+        <br />
+        <Heading size={"lg"}>Created Problems</Heading>
+        <br />
+        {problems.length > 0 ? (
+          <CustomTable
+            data={problems.map((problem: Problem) => {
+              return {
+                problem: (
+                  <a href={`/problems/${problem.id}`}>
+                    #{problem.id} {problem.specification.title}
+                  </a>
+                ),
+                difficulty: problem.specification.difficulty.toLowerCase(),
+                totalRatings: problem.rating.numberOfRatings,
+                avgRating: problem.rating.numberOfRatings
+                  ? Math.round(
+                      (problem.rating.totalRating /
+                        problem.rating.numberOfRatings) *
+                        10
+                    ) / 10
+                  : "N/A",
+              };
+            })}
+            columns={[
+              {
+                Header: "Problem",
+                accessor: "problem",
+              },
+              {
+                Header: "Difficulty",
+                accessor: "difficulty",
+              },
+              {
+                Header: "Total Ratings",
+                accessor: "totalRatings",
+              },
+              {
+                Header: "Average Rating",
+                accessor: "avgRating",
+              },
+            ]}
+          />
+        ) : (
+          <>
+            <Text fontWeight={"bold"}>No Created Problems</Text>
+            <br />
+          </>
+        )}
+      </Box>
+    </>
   );
 };
 

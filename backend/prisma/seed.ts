34e4cd2baf288cb4a0f41412c3b4e8d2ce3aeffd
zaini <alimzaini@outlook.com>
2022-03-19 07:25:22 +0000

    Add more problems to seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -116,60 +116,148 @@ process.stdin.on("data", buffer => {
       },
     },
   });
-  //   const problem2 = await prisma.problem.create({
-  //     data: {
-  //       userId: 2,
-  //       specification: {
-  //         title: "Two Sum",
-  //         difficulty: Difficulty.Medium,
-  //         description: "Add two numbers together and return the result",
-  //         initialCode: JSON.stringify({
-  //           71: `#!/bin/python3
 
-  // def add(a, b):
-  //   return a + b
+  const problem2 = await prisma.problem.create({
+    data: {
+      creator: {
+        connect: {
+          id: 1,
+        },
+      },
+      specification: {
+        create: {
+          title: "Subtraction",
+          difficulty: Difficulty.Easy,
+          description:
+            "Subtract two numbers and return the result.\n\n## Example 1\n`Input: 2 1`\n\n`Output: 1`\n\n## Example 2\n`Input: 5 7`\n\n`Output: -2`",
+          initialCode: JSON.stringify({
+            71: `#!/bin/python3
+  
+def subtract(a, b):
+  return a - b
 
-  // if __name__ == "__main__":
-  //   stdin = input()
-  //   a, b = stdin.split()
-  //   a, b = int(a), int(b)
-  //   print(add(a, b))
-  //           `,
-  //           27: `javacode`,
-  //         }),
-  //         testCases: [
-  //           { id: "1", stdin: "10 22", expectedOutput: "32", isHidden: false },
-  //         ],
-  //       },
-  //     },
-  //   });
-  //   const problem3 = await prisma.problem.create({
-  //     data: {
-  //       userId: 3,
-  //       specification: {
-  //         title: "Three Sum",
-  //         difficulty: Difficulty.Hard,
-  //         description: "Add two numbers together and return the result",
-  //         initialCode: JSON.stringify({
-  //           71: `#!/bin/python3
+if __name__ == "__main__":
+  stdin = input()
+  a, b = stdin.split()
+  a, b = int(a), int(b)
+  print(subtract(a, b))
+`,
+            63: `const subtract = (a, b) => {
+  return a - b;
+}
 
-  // def add(a, b):
-  //   return a + b
+process.stdin.on("data", buffer => {
+  const ab = (buffer + "").split(" ");
+  const a = parseInt(ab[0]);
+  const b = parseInt(ab[1]);
+  console.log(subtract(a, b));
+});
+`,
+          }),
+          testCases: {
+            createMany: {
+              data: [
+                {
+                  stdin: "22 10",
+                  expectedOutput: "12",
+                  isHidden: false,
+                  userId: 1,
+                },
+                {
+                  stdin: "20 10",
+                  expectedOutput: "10",
+                  isHidden: false,
+                  userId: 1,
+                },
+                {
+                  stdin: "70 20",
+                  expectedOutput: "50",
+                  isHidden: true,
+                  userId: 1,
+                },
+                {
+                  stdin: "242323 22342340",
+                  expectedOutput: "-22100017",
+                  isHidden: true,
+                  userId: 1,
+                },
+              ],
+            },
+          },
+        },
+      },
+    },
+  });
 
-  // if __name__ == "__main__":
-  //   stdin = input()
-  //   a, b = stdin.split()
-  //   a, b = int(a), int(b)
-  //   print(add(a, b))
-  //           `,
-  //           27: `javacode`,
-  //         }),
-  //         testCases: [
-  //           { id: "1", stdin: "10 22", expectedOutput: "32", isHidden: false },
-  //         ],
-  //       },
-  //     },
-  //   });
+  const problem3 = await prisma.problem.create({
+    data: {
+      creator: {
+        connect: {
+          id: 1,
+        },
+      },
+      specification: {
+        create: {
+          title: "Double Word",
+          difficulty: Difficulty.Easy,
+          description:
+            'Return a word concatenated with itself.\n\n## Example 1\n`Input: "hello"`\n\n`Output: "hellohello"`\n',
+          initialCode: JSON.stringify({
+            71: `#!/bin/python3
+  
+def doubleup(word):
+  return word + word
+
+if __name__ == "__main__":
+  stdin = input()
+  word = stdin
+  print(doubleup(word))
+`,
+            63: `const doubleup = (word) => {
+  return \`\${word}\${word}\`;
+}
+
+process.stdin.on("data", buffer => {
+  const word = (buffer + "");
+  console.log(doubleup(word));
+});
+`,
+          }),
+          testCases: {
+            createMany: {
+              data: [
+                {
+                  stdin: "hello",
+                  expectedOutput: "hellohello",
+                  isHidden: false,
+                  userId: 1,
+                },
+                {
+                  stdin: "big work",
+                  expectedOutput: "big workbig work",
+                  isHidden: false,
+                  userId: 1,
+                },
+                {
+                  stdin: "xx",
+                  expectedOutput: "xxxx",
+                  isHidden: true,
+                  userId: 1,
+                },
+                {
+                  stdin: "test",
+                  expectedOutput: "testtest",
+                  isHidden: true,
+                  userId: 1,
+                },
+              ],
+            },
+          },
+        },
+      },
+    },
+  });
+
   const allProblems = await prisma.assignment.findMany();
   logger.info("Successfully seeded problems");
   logger.debug(JSON.stringify(allProblems));
@@ -209,9 +297,10 @@ process.stdin.on("data", buffer => {
   await prisma.problemsOnAssignments.createMany({
     data: [
       { problemId: problem1.id, assignmentId: 1 },
-      // { problemId: problem3.id, assignmentId: 1 },
-      // { problemId: problem3.id, assignmentId: 2 },
-      // { problemId: problem2.id, assignmentId: 3 },
+      { problemId: problem2.id, assignmentId: 1 },
+      { problemId: problem3.id, assignmentId: 1 },
+      { problemId: problem3.id, assignmentId: 2 },
+      { problemId: problem2.id, assignmentId: 3 },
       { problemId: problem1.id, assignmentId: 4 },
     ],
   });
@@ -224,13 +313,7 @@ process.stdin.on("data", buffer => {
     data: [
       { problemId: problem1.id, userId: 1, score: 1 },
       { problemId: problem1.id, userId: 2, score: 2 },
-      // { problemId: problem2.id, userId: 1, score: 3 },
-      // { problemId: problem2.id, userId: 2, score: 4 },
-      // { problemId: problem3.id, userId: 1, score: 5 },
-      // { problemId: problem3.id, userId: 2, score: 5 },
       { problemId: problem1.id, userId: 3, score: 5 },
-      // { problemId: problem2.id, userId: 3, score: 5 },
-      // { problemId: problem3.id, userId: 3, score: 5 },
     ],
   });
   const ratings = await prisma.rating.findMany();

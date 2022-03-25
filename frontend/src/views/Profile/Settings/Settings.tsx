57097e0c59e1211c07457267fa05b32c941ead5a
zaini <alimzaini@outlook.com>
2022-03-25 12:42:18 +0000

    Add set organisation ID to account settings

--- frontend/src/views/Profile/Settings/Settings.tsx
+++ frontend/src/views/Profile/Settings/Settings.tsx
@@ -1,38 +1,35 @@
+import React, { useContext, useState } from "react";
 import {
   Box,
   Button,
-  Code,
   Heading,
   Input,
+  InputGroup,
+  InputLeftAddon,
+  Text,
   useDisclosure,
 } from "@chakra-ui/react";
-import React, { useContext, useState } from "react";
 import { AuthContext } from "../../../context/Auth";
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-} from "@chakra-ui/react";
 import { useMutation, gql } from "@apollo/client";
+import DeleteAccountModal from "../../../components/DeleteAccountModal/DeleteAccountModal";
 
-const DELETE_USER = gql`
-  mutation deleteUser($userId: ID!, $username: String!) {
-    deleteUser(userId: $userId, username: $username)
+const SET_ORGANISATION_ID = gql`
+  mutation setOrganisationId($organisationId: String!) {
+    setOrganisationId(organisationId: $organisationId)
   }
 `;
 
 const Settings = () => {
-  const { user, logout }: any = useContext(AuthContext);
+  const { user }: any = useContext(AuthContext);
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [username, setUsername] = useState("");
+  const [organisationId, setOrganisationId] = useState(
+    user.organisationId || ""
+  );
 
-  const [deleteUser] = useMutation(DELETE_USER, {
-    onCompleted: ({ deleteUser }) => {
-      logout();
+  const [runSetOrganisationId] = useMutation(SET_ORGANISATION_ID, {
+    onCompleted: () => {
+      window.alert(`Successfully updated organisation ID.`);
+      window.location.reload();
     },
     onError(err) {
       const message =
@@ -40,63 +37,52 @@ const Settings = () => {
           err.graphQLErrors[0] &&
           err.graphQLErrors[0].message) ||
         err.message;
-      window.alert(`Failed to delete user. \n\n${message}`);
+      window.alert(`Failed to set organisation ID. \n\n${message}`);
     },
   });
 
   return (
     <>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Delete Account</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Box>
-              Account details:
-              <Box backgroundColor={"#EDF2F7"}>{JSON.stringify(user)}</Box>
-              <br />
-              <b>Enter username to confirm deletion:</b>
-              <Input
-                type="text"
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder={user.username}
-              />
-              <br />
-              <br />
-              <Code textAlign={"center"}>
-                <b>WARNING:</b> This will delete your account, all submission,
-                all assignment submissions, all classrooms and problems. You
-                cannot recover these after deletion.
-              </Code>
-            </Box>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button
-              colorScheme="red"
-              onClick={() => {
-                deleteUser({
-                  variables: {
-                    userId: user.id,
-                    username,
-                  },
-                });
-              }}
-            >
-              Confirm Account Deletion
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      <DeleteAccountModal isOpen={isOpen} onClose={onClose} />
       <Box px={"12.5%"} py={8}>
         <Heading>Account Settings</Heading>
         <br />
         <Button onClick={onOpen} colorScheme={"red"}>
           Delete Account
+        </Button>
+        <br />
+        <br />
+        <Heading size={"md"}>Organisation Identifier</Heading>
+        <Text>
+          Your organisation ID is a unique identifier for other platforms, such
+          as a student ID number, email or candidate reference number.
+        </Text>
+        <br />
+        <InputGroup>
+          <InputLeftAddon children="Current Organisation ID" />
+          <Input readOnly={true} value={`${user.organisationId || "N/A"}`} />
+        </InputGroup>
+        <br />
+        <InputGroup>
+          <InputLeftAddon children="New Organisation ID" />
+          <Input
+            value={organisationId}
+            placeholder={`${user.organisationId || "N/A"}`}
+            onChange={(e) => setOrganisationId(e.target.value)}
+          />
+        </InputGroup>
+        <br />
+        <Button
+          onClick={() =>
+            runSetOrganisationId({
+              variables: {
+                organisationId,
+              },
+            })
+          }
+          colorScheme={"blue"}
+        >
+          Update Organisation ID
         </Button>
       </Box>
     </>

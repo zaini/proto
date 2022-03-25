0c253a7e71aaa7b1ca962de63b4913df45f5ded9
zaini <alimzaini@outlook.com>
2022-03-25 11:21:16 +0000

    Show in how many days assignment is due

--- frontend/src/components/AssignmentBox/AssignmentBox.tsx
+++ frontend/src/components/AssignmentBox/AssignmentBox.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { Box, Text, Button } from "@chakra-ui/react";
+import { Box, Text, Button, Tooltip } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { Assignment } from "../../gql-types";
+
+const rtf = new Intl.RelativeTimeFormat("en", { style: "narrow" });
 
 const AssignmentBox = ({ assignment }: { assignment: Assignment }) => {
   return (
     <Box
       p={2}
-      minw={"100%"}
+      minW={"280px"}
       backgroundColor={"#EEEEEE"}
       fontWeight={700}
       borderRadius={5}
@@ -18,9 +20,21 @@ const AssignmentBox = ({ assignment }: { assignment: Assignment }) => {
       <Text fontSize="md" opacity={"50%"}>
         {assignment.classroom.name}
       </Text>
-      <Text fontSize="md">{`Due on ${new Date(
-        parseInt(assignment.dueDate)
-      ).toLocaleString()}`}</Text>
+      <Tooltip
+        label={`Due on ${new Date(
+          parseInt(assignment.dueDate)
+        ).toLocaleString()}`}
+      >
+        <Text fontSize="md">{`Due in ${rtf.format(
+          parseInt(
+            `${
+              (parseInt(assignment.dueDate) - new Date().getTime()) /
+              (24 * 60 * 60 * 1000)
+            }`
+          ),
+          "day"
+        )}`}</Text>
+      </Tooltip>
       <Box textAlign="right" mt={4}>
         <Button
           as={Link}
